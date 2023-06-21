e3777e52b636cc6b034d4f2e913f41a70826a2de
allez_ryan_venga <120065584+MiAmigoRyan@users.noreply.github.com>
2023-06-21 10:51:00 -0600

    create Spring Boot REST project, add JPA project dependcy

---
+++ LeagueOlympia/src/main/java/com/skilldistillery/leagueolympia/LeagueOlympiaApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.leagueolympia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeagueOlympiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeagueOlympiaApplication.class, args);
+	}
+
+}
---
+++ LeagueOlympia/src/test/java/com/skilldistillery/leagueolympia/LeagueOlympiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.leagueolympia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeagueOlympiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
