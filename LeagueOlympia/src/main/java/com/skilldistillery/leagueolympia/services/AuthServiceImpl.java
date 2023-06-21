8c7cab20f47f0b52744f08449ba826523f78bb85
allez_ryan_venga <120065584+MiAmigoRyan@users.noreply.github.com>
2023-06-21 11:08:37 -0600

    created AuthController, AuthService, AuthServiceImpl, UserRepo, SecurityConfig; added PasswordEncoder to LeagueOlympiaApplication

---
+++ LeagueOlympia/src/main/java/com/skilldistillery/leagueolympia/services/AuthServiceImpl.java
@@ -0,0 +1,36 @@
+package com.skilldistillery.leagueolympia.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.skilldistillery.leagueolympia.entities.User;
+import com.skilldistillery.leagueolympia.repositories.UserRepository;
+@Service
+public class AuthServiceImpl implements AuthService {
+
+	@Autowired
+	private UserRepository userRepo;
+	
+	@Autowired
+	private PasswordEncoder encoder;
+	
+	@Override
+	public User register(User user) {
+		//FIX ME
+//		String encryptedPassword = encoder.encode(user.getPassword());
+//		user.setPassword(encryptedPassword);
+//		
+//		user.setEnabled(true);
+//		user.setRole("standard");
+//		
+		return userRepo.saveAndFlush(user);
+	}
+
+	@Override
+	public User getUserByUsername(String username) {
+		
+		return userRepo.findByUsername(username);
+	}
+
+}
