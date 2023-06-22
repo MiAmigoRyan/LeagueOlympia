e121fefb1cf18f4998550d80439bac085f444d7a
allez_ryan_venga <120065584+MiAmigoRyan@users.noreply.github.com>
2023-06-22 14:06:33 -0600

    added tests for entities, updated entities

---
+++ JPALeagueOlympia/src/test/java/com/skilldistillery/leagueolympia/entities/TeamTest.java
@@ -0,0 +1,45 @@
+package com.skilldistillery.leagueolympia.entities;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class TeamTest {
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	private Team team;
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("JPALeagueOlympia");
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+		emf.close();
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		team = em.find(Team.class, 1);
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void test_Team() {
+		assertNotNull(team);
+//			assertEquals("admin", user.getUsername());
+	}
+}
