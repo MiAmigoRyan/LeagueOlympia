1e23eca7a952348dad1463a7dfbca604ad31166f
allez_ryan_venga <120065584+MiAmigoRyan@users.noreply.github.com>
2023-06-22 13:54:38 -0600

    added OlympiadCategory

---
+++ JPALeagueOlympia/src/main/java/com/skilldistillery/leagueolympia/entities/OlympaidCategory.java
@@ -0,0 +1,77 @@
+package com.skilldistillery.leagueolympia.entities;
+
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class OlympaidCategory {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private String name;
+	
+	private String description;
+
+	public OlympaidCategory() {
+		super();
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(description, id, name);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		OlympaidCategory other = (OlympaidCategory) obj;
+		return Objects.equals(description, other.description) && id == other.id && Objects.equals(name, other.name);
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("OlympaidCategory [id=").append(id).append(", name=").append(name).append(", description=")
+				.append(description).append("]");
+		return builder.toString();
+	}
+	
+	
+	
+	
+}
