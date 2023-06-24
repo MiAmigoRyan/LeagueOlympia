d836aeba244d3ef24acc7e139492d4cef243af99
Spencer Williams <srwilliams349@gmail.com>
2023-06-24 12:55:18 -0400

    register, login, logout, navbar stubbed out and related forms built out

--- ngLeagueOlympia/src/app/app-routing.module.ts
+++ ngLeagueOlympia/src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LeagueListComponent } from './components/league-list/league-list.component';
+import { LoginComponent } from './components/login/login.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { RegisterComponent } from './components/register/register.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'leagues', component: LeagueListComponent },
+  // { path: 'todo/:id', component: TodoListComponent },
+  { path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {useHash: true})],
---
+++ ngLeagueOlympia/src/app/components/login/login.component.ts
@@ -0,0 +1,43 @@
+import { Router } from '@angular/router';
+import { Component } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+import { User } from 'src/app/models/user';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+
+  loginUser: User = new User();
+
+    constructor(
+      private auth: AuthService,
+      private router: Router
+    ){}
+
+    login(user: User) {
+      console.log('logging in user:');
+      console.log(user);
+
+      this.auth.login(user.username, user.password).subscribe( {
+        next: (loggedInUser) => {
+          // this.router.navigateByUrl('/todo');
+          // this.reloadRoute('/todo');
+        },
+        error: (loginFailure) => {
+          console.error('LoginComponent.login(): login failed');
+          console.error(loginFailure);
+          this.router.navigateByUrl('register');
+        }
+      });
+    }
+
+    reloadRoute(route: string) {
+      this.router.navigateByUrl('/', {skipLocationChange: true}).then( () => {
+        this.router.navigate([route]);
+      })
+    }
+
+}
