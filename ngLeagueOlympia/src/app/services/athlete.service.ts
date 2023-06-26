4a740136ff4933b90063038b261b83880adb8b5d
Spencer Williams <srwilliams349@gmail.com>
2023-06-26 19:02:40 -0400

    unreg users can view athlete list

--- ngLeagueOlympia/src/app/services/athlete.service.ts
+++ ngLeagueOlympia/src/app/services/athlete.service.ts
@@ -1,17 +1,17 @@
+import { Athlete } from 'src/app/models/athlete';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, catchError, throwError } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Team } from '../models/team';
 import { AuthService } from './auth.service';
-import { Athlete } from '../models/athlete';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AthleteService {
   private url = environment.baseUrl + 'api/athletes';
-
+  athletes: Athlete[] = [];
 
   constructor(
     private http: HttpClient,
@@ -19,7 +19,8 @@ export class AthleteService {
   ) { }
 
   public index(): Observable<Athlete[]> {
-    return this.http.get<Athlete[]>(this.url, this.getHttpOptions()).pipe(
+    console.log(this.athletes);
+    return this.http.get<Athlete[]>(this.url).pipe(
       catchError((err: any) => {
         console.error('Error GETing athlete list');
         return throwError(
