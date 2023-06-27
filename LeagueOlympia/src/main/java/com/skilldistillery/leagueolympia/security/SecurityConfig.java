4288bd2a24199894bdfc7f8e7c045da0a50f9e9f
allez_ryan_venga <120065584+MiAmigoRyan@users.noreply.github.com>
2023-06-27 09:07:51 -0600

    created hole in sec config for athleteEvents

--- LeagueOlympia/src/main/java/com/skilldistillery/leagueolympia/security/SecurityConfig.java
+++ LeagueOlympia/src/main/java/com/skilldistillery/leagueolympia/security/SecurityConfig.java
@@ -33,6 +33,7 @@ public class SecurityConfig {
         .antMatchers(HttpMethod.GET, "/api/leagues").permitAll() // GET Requests for Unregistered Users to view Leagues permitted.
         .antMatchers(HttpMethod.GET, "/api/events").permitAll() // GET Requests for Unregistered Users to view Events permitted.
         .antMatchers(HttpMethod.GET, "/api/athletes").permitAll() // GET Requests for Unregistered Users to view Athletes permitted.
+        .antMatchers(HttpMethod.GET, "/api/athleteEvents").permitAll() // GET Requests for Unregistered Users to view Athletes permitted.
         .antMatchers("/api/**").authenticated() // Requests for our REST API must be authorized.
         .anyRequest().permitAll()               // All other requests are allowed without authentication.
         .and()
